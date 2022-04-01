045a2335cf8b0b147d20f082ce82c6f8e4e9da1b
Chris Chasm <chris@chasm.solutions>
2022-04-01 07:11:43 -0600

    Add Moravian video

--- pages/assets/pg.js
+++ pages/assets/pg.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($){
 
   /* video modal */
   $('#video-link-icon').on('click', function(){
-    $('#demo_video .modal-body').html('<iframe style="width:100%;max-width:600px;height:300px;" src="https://www.youtube.com/embed/xIZbADbgkwA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+    $('#demo_video .modal-body').html('<div style="padding:75% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/694876675?h=fa791a640e&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="moravian-prayer-challenge"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>')
     $('#demo_video').modal('show')
   })
 
