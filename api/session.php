7dd1ba181e5ca3f31476caea098b485516b0e844
Nath <nathinabob@gmail.com>
2023-01-19 13:56:42 +0000

    feat: add a standalone login page for redirecting through

---
+++ api/session.php
@@ -0,0 +1,5 @@
+<?php
+
+if ( !defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly.
+
+class PG_Session_API {}
---
+++ pages/user/user-login.php
@@ -0,0 +1,135 @@
+<?php
+
+/**
+ * Displays a page for the user to login/register and recover password etc.
+ *
+ * Any part of the site can send the user to the login page with an encoded redirect url to get back to where they were,
+ * after the login/registration.
+ */
+class PG_User_Login_Registration extends DT_Magic_Url_Base {
+
+    public $page_title = 'User Login';
+    public $root = 'user_app';
+    public $type = 'login';
+    public $post_type = 'user';
+
+    private static $_instance = null;
+    public static function instance() {
+        if ( is_null( self::$_instance ) ) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    } // End instance()
+
+    public function __construct() {
+        parent::__construct();
+
+        /**
+         * tests if other URL
+         */
+        $url = dt_get_url_path();
+        if ( strpos( $url, $this->root . '/' . $this->type ) === false ) {
+            return;
+        }
+        /**
+         * tests magic link parts are registered and have valid elements
+         */
+        if ( !$this->check_parts_match( false ) ){
+            return;
+        }
+
+        // load if valid url
+        add_action( 'dt_blank_body', [ $this, 'body' ] ); // body for no post key
+
+        add_filter( 'dt_magic_url_base_allowed_css', [ $this, 'dt_magic_url_base_allowed_css' ], 10, 1 );
+        add_filter( 'dt_magic_url_base_allowed_js', [ $this, 'dt_magic_url_base_allowed_js' ], 10, 1 );
+        add_action( 'wp_enqueue_scripts', [ $this, 'wp_enqueue_scripts' ], 99 );
+    }
+
+    public function dt_magic_url_base_allowed_css( $allowed_css ) {
+        return [
+            'porch-user-style-css',
+            'jquery-ui-site-css',
+            'foundations-css',
+        ];
+    }
+
+    public function dt_magic_url_base_allowed_js( $allowed_js ) {
+        return array_merge( $allowed_js, [
+            'jquery',
+            'jquery-ui',
+            'foundations-js',
+            'porch-user-site-js',
+        ]);
+    }
+
+    public function wp_enqueue_scripts() {}
+
+    public function header_javascript(){
+        require_once( trailingslashit( plugin_dir_path( __DIR__ ) ) . 'assets/header.php' );
+
+        $user_id = get_current_user_id();
+
+        ?>
+        <script>
+            let jsObject = [<?php echo json_encode([
+                'root' => esc_url_raw( rest_url() ),
+                'nonce' => wp_create_nonce( 'wp_rest' ),
+                'parts' => $this->parts,
+                'is_logged_in' => is_user_logged_in() ? 1 : 0,
+                'logout_url' => esc_url( wp_logout_url( '/' ) )
+            ]) ?>][0]
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/js-cookie@rc/dist/js.cookie.min.js?ver=3"></script>
+        <style>
+            #login_form input {
+                padding:.5em;
+            }
+        </style>
+        <?php
+    }
+
+    public function footer_javascript(){
+        require_once( trailingslashit( plugin_dir_path( __DIR__ ) ) . 'assets/footer.php' );
+    }
+
+
+    public function body() {
+        require_once( trailingslashit( plugin_dir_path( __DIR__ ) ) . '/assets/nav.php' );
+
+        ?>
+
+        <section class="page-section" data-section="login" id="section-login">
+            <div class="container">
+                <div class="row justify-content-md-center text-center">
+                    <div class="col-lg-7 flow" id="pg_content">
+                        <form id="login_form">
+                            <p>
+                                <h2 class="header-border-top">Login</h2>
+                            </p>
+                            <p>
+                                Email<br>
+                                <input type="text" id="pg_input_email"  />
+                            </p>
+                            <p>
+                                Password<br>
+                                <input type="password" id="pg_input_password" />
+                            </p>
+                            <p>
+                                <button class="btn btn-outline-dark" type="button" id="submit_button">Submit</button>
+                            </p>
+                            <span class="loading-spinner"></span>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+        <?php
+
+    }
+
+}
+PG_User_Login_Registration::instance();
