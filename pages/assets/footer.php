8b22cf946d7059c2749e4bb838d0ca4d48075faf
corsacca <corsacca@gmail.com>
2024-05-10 14:33:52 +0100

    Add Umami tracking

--- pages/assets/footer.php
+++ pages/assets/footer.php
@@ -1,1 +1,2 @@
 <?php require_once( __DIR__ . '/share-modal.php' ) ?>
+<script defer src="https://umami.gospelambition.com/script.js" data-website-id="04dbb157-5810-4c12-9522-4c8e02d702ea"></script>
