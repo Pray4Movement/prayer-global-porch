6f5d289a716b82d79f99da48b06905946c71da46
corsacca <corsacca@gmail.com>
2024-10-14 11:58:24 +0100

    save last saved log id per relay

--- api/pray-sync.php
+++ api/pray-sync.php
@@ -22,18 +22,20 @@ class PG_Prayer_API {
 
     public function trigger( WP_REST_Request $request ) {
         $params = $request->get_params();
-        $relay = $params['relay'];
-        if ( empty( $relay ) ){
+        $relay_id = $params['relay'];
+        if ( empty( $relay_id ) ){
             return new WP_Error( 'missing_relay', 'Missing relay parameter', [ 'status' => 400 ] );
         }
         $url = 'http://api.prayer.global/logs';
-        $last_saved_id = get_option( 'pg_sync_last_saved_id', 0 );
+
+        //get the id of the last saved location, get logs since then
+        $last_saved_id = get_option( 'pg_sync_last_saved_id_' . $relay_id, 0 );
         $url .= '?last_id=' . $last_saved_id;
         $response = wp_remote_get( $url );
         $body = wp_remote_retrieve_body( $response );
         $data = json_decode( $body, true );
         if ( !empty( $data['total'] ) ){
-            $this->save_logs( $data, $relay );
+            $this->save_logs( $data, $relay_id );
         }
         return $data['total'];
     }
@@ -44,10 +46,12 @@ class PG_Prayer_API {
 
         $current_lap = pg_current_custom_lap( $relay_id );
 
+        //split logs into groups of 500 to avoid max query length
         $chunked_arrays = array_chunk( $logs['rows'], 500 );
         foreach ( $chunked_arrays as $chunked_array ){
             $sql = "INSERT INTO $wpdb->dt_reports (post_id, post_type, type, subtype, value, grid_id, timestamp) VALUES ";
             foreach ( $chunked_array as $log ){
+                //check and generate a new lap if needed
                 if ( floor( $log['id'] / 4700 ) > $current_lap['lap_number'] ){
                     pg_generate_new_custom_prayer_lap( $current_lap['post_id'] );
                     $current_lap = pg_current_custom_lap( $relay_id );
@@ -58,7 +62,7 @@ class PG_Prayer_API {
             }
             $sql = rtrim( $sql, ',' );
             $wpdb->query( $sql );
-            update_option( 'pg_sync_last_saved_id' , $chunked_array[ count( $chunked_array ) - 1 ]['id'] );
+            update_option( 'pg_sync_last_saved_id_' . $relay_id , $chunked_array[ count( $chunked_array ) - 1 ]['id'] );
         }
     }
 }
