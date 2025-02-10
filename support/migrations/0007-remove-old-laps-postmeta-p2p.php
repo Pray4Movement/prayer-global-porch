23e34392658017e57450343972a6bd87754189c4
Nath <nathinabob@gmail.com>
2025-02-10 17:23:35 +0000

    feat: add migration to remove all laps and their postmeta + p2p

---
+++ support/migrations/0007-remove-old-laps-postmeta-p2p.php
@@ -0,0 +1,81 @@
+<?php
+declare(strict_types=1);
+
+if ( !defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly
+
+
+require_once( 'abstract.php' );
+
+/**
+ * Class Disciple_Tools_Migration_0000
+ */
+class Prayer_Global_Migration_0007 extends Prayer_Global_Migration {
+
+    /**
+     * @throws \Exception  Got error when creating table $name.
+     */
+    public function up() {
+        global $wpdb;
+        $wpdb->postmeta = $wpdb->prefix . 'postmeta';
+        $wpdb->posts = $wpdb->prefix . 'posts';
+        $wpdb->p2p = $wpdb->prefix . 'p2p';
+
+        /* Remove old lap postmeta */
+        $query = $wpdb->query( "
+            DELETE wp_postmeta FROM $wpdb->postmeta
+            JOIN $wpdb->posts p ON ID = post_id
+            WHERE p.post_type = 'laps'
+        " );
+        if ( $query === false ) {
+            throw new \Exception( "Got error when updating table $wpdb->dt_reports." );
+        }
+        /* Remove old lap p2p */
+        $query = $wpdb->query( "
+            DELETE wp_p2p FROM $wpdb->p2p
+            JOIN $wpdb->posts ON (ID = p2p_from OR ID = p2p_to)
+            WHERE post_type = 'laps'
+        " );
+        if ( $query === false ) {
+            throw new \Exception( "Got error when updating table $wpdb->dt_reports." );
+        }
+        /* Remove old laps */
+
+        $query = $wpdb->query( "
+            DELETE FROM $wpdb->posts
+            WHERE post_type = 'laps'
+        " );
+        if ( $query === false ) {
+            throw new \Exception( "Got error when updating table $wpdb->dt_reports." );
+        }
+    }
+
+    /**
+     * @throws \Exception  Got error when dropping table $name.
+     */
+    public function down() {
+        global $wpdb;
+        $wpdb->dt_relays = $wpdb->prefix . 'dt_relays';
+
+        $query = $wpdb->query( "
+            ALTER TABLE $wpdb->dt_relays
+            RENAME COLUMN relay_key TO relay_id;
+        " );
+        if ( $query === false ) {
+            throw new \Exception( "Got error when updating table $wpdb->dt_reports." );
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function get_expected_tables(): array {
+        return [];
+    }
+
+    /**
+     * Test function
+     */
+    public function test() {
+        $this->test_expected_tables();
+    }
+}
--- support/migrations/class-migration-engine.php
+++ support/migrations/class-migration-engine.php
@@ -12,7 +12,7 @@ if ( !defined( 'ABSPATH' ) ) {
 class Prayer_Global_Migration_Engine
 {
 
-    public static $migration_number = 6;
+    public static $migration_number = 7;
 
     protected static $migrations = null;
 
