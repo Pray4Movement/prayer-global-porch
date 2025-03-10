6cf10f128d0f627d56b3fc4d8a23fcbd1b60dda3
Nath <nathinabob@gmail.com>
2025-03-10 17:44:33 +0000

    feat: add translation strings to activity page

--- pages/assets/js/src/profile-pages/pg-relays.ts
+++ pages/assets/js/src/profile-pages/pg-relays.ts
@@ -9,6 +9,15 @@ export class PgRelays extends PageBase {
   translations: any = window.jsObject.translations;
 
   render() {
-    return html` <h2>My Prayer Relays</h2> `;
+    return html`
+      <div class="offcanvas__header align-items-center lh-sm">
+        <button type="button" class="me-auto" @click=${() => history.back()}>
+          <i class="icon pg-chevron-left two-em"></i>
+        </button>
+        <h3 class="mb-0 me-auto">${this.translations.prayer_relays}</h3>
+      </div>
+
+      <div class="white-bg page px-3"></div>
+    `;
   }
 }
