1e42a9fec1b641759eef355c3c4ab6d111ab95e6
Nath <nathinabob@gmail.com>
2025-03-13 09:47:59 +0000

    fix the merge commit that got stuck in the editor

--- pages/assets/js/src/profile-pages/pg-relays.ts
+++ pages/assets/js/src/profile-pages/pg-relays.ts
@@ -8,6 +8,27 @@ export class PgRelays extends PageBase {
   user: User = window.pg_global.user;
   translations: any = window.jsObject.translations;
 
+  constructor() {
+    super();
+
+    fetch(window.pg_global.root + "pg-api/v1/profile/relays", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-WP-Nonce": window.pg_global.nonce,
+      },
+      body: JSON.stringify({
+        data: {
+          user_id: this.user.id,
+        },
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }
+
   render() {
     return html` <h2>My Prayer Relays</h2> `;
   }
