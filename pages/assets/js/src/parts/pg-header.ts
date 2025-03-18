bc9d1ecc8df6dbd733a964be5f08411fb8615bd8
Nath <nathinabob@gmail.com>
2025-03-18 13:11:41 +0000

    fix: make dashboard header go back to the previous page via a link

--- pages/assets/js/src/parts/pg-header.ts
+++ pages/assets/js/src/parts/pg-header.ts
@@ -1,17 +1,25 @@
 import { html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import { OpenElement } from "../profile-pages/open-element";
+import { navigator } from "lit-element-router";
 
 @customElement("pg-header")
-export class PgHeader extends OpenElement {
+export class PgHeader extends navigator(OpenElement) {
   @property({ type: String })
   title: string = "";
+
+  @property({ type: String })
+  backUrl: string = "";
 
   render() {
     return html`
       <div class="offcanvas__header lh-sm">
         <div class="container d-flex align-items-center">
-          <button type="button" class="me-auto" @click=${() => history.back()}>
+          <button
+            type="button"
+            class="me-auto"
+            @click=${() => this.navigate(this.backUrl)}
+          >
             <svg class="icon-md">
               <use
                 href="${window.jsObject.spritesheet_url}#pg-chevron-left"
