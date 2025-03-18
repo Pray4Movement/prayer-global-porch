bb433b94a283b7aaed8a6c26583b89b9568b74d3
corsacca <corsacca@gmail.com>
2025-03-18 13:41:31 +0100

    Update duplicate check parameter in CRM connection URL

--- utilities/login-functions.php
+++ utilities/login-functions.php
@@ -41,7 +41,7 @@ function pg_connect_to_crm(){
     $site_key = md5( $key['token'] . $key['site1'] . $key['site2'] );
     $transfer_token = Site_Link_System::create_transfer_token_for_site( $site_key );
 
-    $url = 'https://' . $key['site2'] . '/wp-json/dt-posts/v2/contacts?duplicate_check=true';
+    $url = 'https://' . $key['site2'] . '/wp-json/dt-posts/v2/contacts?check_for_duplicates=contact_email';
 
     $current_user = wp_get_current_user();
 
