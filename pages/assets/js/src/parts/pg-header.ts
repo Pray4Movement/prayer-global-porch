87d0c06b0d2a1573fc5f3187e003e223eea4bb89
squigglybob <squigglybob@users.noreply.github.com>
2025-03-19 16:36:39 +0000

    Fix/dashboard nav container (#595)
    * fix: add a container to dashboard nav
    
    * fix: make dashboard header go back to the previous page via a link
    
    * revert: add form-group class to email label

--- pages/assets/js/src/parts/pg-header.ts
+++ pages/assets/js/src/parts/pg-header.ts
@@ -1,23 +1,33 @@
 import { html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import { OpenElement } from "../profile-pages/open-element";
+import { navigator } from "lit-element-router";
 
 @customElement("pg-header")
-export class PgHeader extends OpenElement {
+export class PgHeader extends navigator(OpenElement) {
   @property({ type: String })
   title: string = "";
 
+  @property({ type: String })
+  backUrl: string = "";
+
   render() {
     return html`
-      <div class="offcanvas__header align-items-center lh-sm">
-        <button type="button" class="me-auto" @click=${() => history.back()}>
-          <svg class="icon-md">
-            <use
-              href="${window.jsObject.spritesheet_url}#pg-chevron-left"
-            ></use>
-          </svg>
-        </button>
-        <h3 class="mb-0 me-auto">${this.title}</h3>
+      <div class="offcanvas__header lh-sm">
+        <div class="container d-flex align-items-center">
+          <button
+            type="button"
+            class="me-auto"
+            @click=${() => this.navigate(this.backUrl)}
+          >
+            <svg class="icon-md">
+              <use
+                href="${window.jsObject.spritesheet_url}#pg-chevron-left"
+              ></use>
+            </svg>
+          </button>
+          <h3 class="mb-0 me-auto">${this.title}</h3>
+        </div>
       </div>
     `;
   }
