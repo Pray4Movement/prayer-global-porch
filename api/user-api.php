4c5d0e494f2877f2e873021de372f8435dc35eab
Nath <nathinabob@gmail.com>
2025-03-21 16:12:08 +0000

    phpcs

--- api/user-api.php
+++ api/user-api.php
@@ -239,7 +239,7 @@ class PG_User_API {
         }
 
         $body = json_decode( $request->get_body() );
-        if ( !isset( $body->onesignal_user_id ) || !isset( $body->onesignal_external_id ) || !isset( $body->onesignal_subscription_id ) )  {
+        if ( !isset( $body->onesignal_user_id ) || !isset( $body->onesignal_external_id ) || !isset( $body->onesignal_subscription_id ) ) {
             return new WP_Error( __METHOD__, 'Missing required fields', [ 'status' => 400 ] );
         }
 
