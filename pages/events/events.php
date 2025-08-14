d1cb265b2d5eb5c68d7ed99907d053b51548b8b3
Nath <nathinabob@gmail.com>
2025-08-14 13:49:12 +0100

    feat: finish styling the page

--- pages/events/events.php
+++ pages/events/events.php
@@ -72,8 +72,8 @@ class PG_Events extends PG_Public_Page {
         $svgs_url = $svg_manager->get_cached_spritesheet_url( $icons );
         ?>
 
-        <div class="page flow-xmedium">
-            <section class="container">
+        <div class="page">
+            <section class="container py-5">
                 <div class="row justify-content-md-center mb-5 stack-md">
                     <h1 class="text-center"><?php echo esc_html__( 'Using Prayer.Global at your Event', 'prayer-global-porch' ) ?></h1>
                     <p class="text-center font-weight-bold font-italic f-sm"><?php echo esc_html__( 'Prayer.Global has been used by churches and conferences with great success! ', 'prayer-global-porch' ) ?></p>
@@ -102,11 +102,11 @@ class PG_Events extends PG_Public_Page {
                     <a class="d-block text-decoration-none brand-light text-center" href="#what-is-prayer-global"><i class="icon pg-chevron-down icon-small"></i></a>
                 </div>
             </section>
-            <section id="what-is-prayer-global" class="container flow-medium">
+            <section id="what-is-prayer-global" class="container flow-medium | py-5">
                 <h2><?php echo esc_html__( 'What is Prayer.Global?', 'prayer-global-porch' ) ?></h2>
                 <p><?php echo esc_html__( 'Prayer.Global is a powerful tool designed to mobilize strategic, Scripture-based prayer for the fulfillment of the Great Commission. It equips individuals, churches, and conferences to engage in extraordinary prayer—a key mark of disciple-making movements. With Prayer.Global, you and your community can take part in God’s mission to cover the world in prayer.', 'prayer-global-porch' ) ?></p>
             </section>
-            <section id="how-it-works" class="container">
+            <section id="how-it-works" class="container | py-5">
                 <h2><?php echo esc_html__( 'How It Works', 'prayer-global-porch' ) ?></h2>
                 <ul>
                     <li><?php echo esc_html__( 'Prayer.Global has divided the world into 4,770 “states” based on geographical and governmental boundaries.', 'prayer-global-porch' ) ?></li>
@@ -129,7 +129,7 @@ class PG_Events extends PG_Public_Page {
                     </div>
                 </div>
             </section>
-            <section class="container" id="pre-event">
+            <section class="container | py-5" id="pre-event">
                 <h2><?php echo esc_html__( 'Pre-Event Setup', 'prayer-global-porch' ) ?></h2>
                 <p class="font-weight-bold"><?php echo esc_html__( 'We’ve done the hard work to make it easy for you.', 'prayer-global-porch' ) ?></p>
                 <ul>
@@ -169,28 +169,32 @@ class PG_Events extends PG_Public_Page {
                     <a class="btn btn-outline-light d-block w-fit mx-auto py-1" href="#promotion"><?php echo esc_html__( 'Promo resources', 'prayer-global-porch' ) ?></a>
                 </div>
             </section>
-            <section class="container">
+            <section class="container flow-medium | py-5">
                 <h2><?php echo esc_html__( 'Promote in advance of your event', 'prayer-global-porch' ) ?></h2>
                 <p><?php echo esc_html__( 'Give your attendees advance notice of the upcoming Prayer.Global event to encourage their participation and to help them be prepared to pray on the day of your event. Here is a proposed timeline for promotion.', 'prayer-global-porch' ) ?></p>
                 <div class="switcher">
-                    <ul>
+                    <ul class="flex-2">
                         <li><?php echo esc_html__( 'Make an announcement in your event materials, encourage people to bring a device with them.', 'prayer-global-porch' ) ?></li>
                         <li><?php echo esc_html__( 'Send an email at least 3 days before to cast vision and to encourage your event attendees to download the Prayer.Global app. Include links to both app stores.', 'prayer-global-porch' ) ?></li>
                         <li><?php echo esc_html__( 'Send a text the day before the event with a link to Prayer.Global, if you have a texting service.', 'prayer-global-porch' ) ?></li>
                         <li><?php echo esc_html__( 'Provide postcards in seat pockets and at exits to provide more information.', 'prayer-global-porch' ) ?></li>
                     </ul>
-                    <img src="<?php echo esc_url( trailingslashit( plugin_dir_url( __DIR__ ) ) ) ?>assets/images/SingleBanner-Mockup.png" alt="">
+                    <div class="d-flex align-items-center justify-content-center">
+                        <img class="h-22" src="<?php echo esc_url( trailingslashit( plugin_dir_url( __DIR__ ) ) ) ?>assets/images/SingleBanner-Mockup-crop.png" alt="">
+                    </div>
                 </div>
                 <p class="font-italic">
                     <?php echo esc_html__( 'In pre-event promotion, we recommend only promoting the Prayer.Global website and the links for downloading the app. We don’t recommend sharing your custom group URL until the day of the event. This helps ensure better tracking of how many people prayed with you during the event.', 'prayer-global-porch' ) ?>
                 </p>
             </section>
-            <section>
-                <img src="<?php echo esc_url( trailingslashit( plugin_dir_url( __DIR__ ) ) ) ?>assets/images/LargeEvent-Mockup2.jpg" alt="">
-                <div class="container">
+            <section class="parallax | narrow-parallax">
+                <div class="parallax__layer parallax__layer--back" style="background-image: url('<?php echo esc_url( trailingslashit( plugin_dir_url( __DIR__ ) ) ) ?>assets/images/LargeEvent-Mockup2.jpg');"></div>
+            </section>
+            <section class="py-5">
+                <div class="container flow-xmedium">
                     <h2><?php echo esc_html__( 'Hosting your Prayer.Global event', 'prayer-global-porch' ) ?></h2>
                     <p><?php echo esc_html__( 'It is important to give the Prayer.Global challenge prominence. We have broken this goal down into three essential keys for success:', 'prayer-global-porch' ) ?></p>
-                    <ol class="large-numbers">
+                    <ol class="large-numbers | flow-xmedium">
                         <li>
                             <h3><?php echo esc_html__( 'Getting people connected to your group’s prayer lap (5 minutes)', 'prayer-global-porch' ) ?></h3>
                             <p><?php echo esc_html__( 'Here are some suggested methods of facilitating attendee connection:', 'prayer-global-porch' ) ?></p>
@@ -226,42 +230,50 @@ class PG_Events extends PG_Public_Page {
                     </ol>
                 </div>
             </section>
-            <section>
-                <div class="switcher">
-                    <img src="<?php echo esc_url( trailingslashit( plugin_dir_url( __DIR__ ) ) ) ?>assets/images/iphonemockup-invite.jpg" alt="">
-                    <img src="<?php echo esc_url( trailingslashit( plugin_dir_url( __DIR__ ) ) ) ?>assets/images/iphonemockup-invite.jpg" alt="">
-                    <img src="<?php echo esc_url( trailingslashit( plugin_dir_url( __DIR__ ) ) ) ?>assets/images/iphonemockup-invite.jpg" alt="">
+            <section class="container | py-5">
+                <div class="switcher | triple-image">
+                    <div class="d-flex align-items-center justify-content-center">
+                        <img class="circle-image" src="<?php echo esc_url( trailingslashit( plugin_dir_url( __DIR__ ) ) ) ?>assets/images/iphonemockup-invite.jpg" alt="">
+                    </div>
+                    <div class="d-flex align-items-center justify-content-center flex-1_5">
+                        <img class="circle-image" src="<?php echo esc_url( trailingslashit( plugin_dir_url( __DIR__ ) ) ) ?>assets/images/iphonemockup-invite.jpg" alt="">
+                    </div>
+                    <div class="d-flex align-items-center justify-content-center">
+                        <img class="circle-image" src="<?php echo esc_url( trailingslashit( plugin_dir_url( __DIR__ ) ) ) ?>assets/images/iphonemockup-invite.jpg" alt="">
+                    </div>
                 </div>
             </section>
-            <section id="helpful-tips" class="bg-brand-light">
+            <section id="helpful-tips" class="brand-lightest-bg | py-5 white">
                 <div class="container">
                     <h2><?php echo esc_html__( 'Helpful Tips', 'prayer-global-porch' ) ?></h2>
                     <div class="switcher">
-                        <ul>
+                        <ul class="flex-1_5">
                             <li><?php echo esc_html__( 'Personally use Prayer.Global before promoting it. Consider initiating leadership, ushers, and volunteers with a Prayer.Global experience before the event, as well.', 'prayer-global-porch' ) ?></li>
                             <li><?php echo esc_html__( 'Set a specific amount of time for prayer. For reference, setting aside 10 minutes will result in each person praying for about 7 states.', 'prayer-global-porch' ) ?></li>
                             <li><?php echo esc_html__( 'Recruit a few technology helpers to be positioned around the room when your prayer time begins. They can help people troubleshoot any issues with their device or access the correct prayer relay.', 'prayer-global-porch' ) ?></li>
                             <li><?php echo esc_html__( 'Encourage people to pray out loud. Model this from the stage and play some soft music during the prayer time. This will help the room not feel empty while also encouraging participation.', 'prayer-global-porch' ) ?></li>
                             <li><?php echo esc_html__( 'Show a prayer prompt session on the main screens so those without devices can pray along, too.', 'prayer-global-porch' ) ?></li>
                         </ul>
-                        <img src="<?php echo esc_url( trailingslashit( plugin_dir_url( __DIR__ ) ) ) ?>assets/images/PG-2Phones-new.png" alt="">
+                        <div class="d-flex align-items-center justify-content-center">
+                            <img src="<?php echo esc_url( trailingslashit( plugin_dir_url( __DIR__ ) ) ) ?>assets/images/PG-2Phones-new.png" alt="">
+                        </div>
                     </div>
                 </div>
             </section>
-            <section id="post-event" class="container">
+            <section id="post-event" class="container | mt-5">
                 <h2><?php echo esc_html__( 'After the event & Beyond', 'prayer-global-porch' ) ?></h2>
                 <p><?php echo esc_html__( 'Based on how your Prayer.Global event experience goes, provide follow up information, texts or emails to encourage people to keep praying and to celebrate once your lap is complete, if not completed during the event.  Keep the vision and goal of daily prayer for the nations in front of them.', 'prayer-global-porch' ) ?></p>
             </section>
-            <section id="thankyou" class="container">
+            <section id="thankyou" class="container | mt-5">
                 <h2><?php echo esc_html__( 'Thank you for Partnering with us', 'prayer-global-porch' ) ?></h2>
                 <p><?php echo esc_html__( 'Thank you for taking the time to explore how to host a Prayer.Global event. By following these steps, you are creating a powerful opportunity for your conference attendees to unite in prayer for the nations. As you guide others in lifting their voices to God, may you see hearts ignited with a passion to pray boldly and consistently — not just during your event, but far beyond it.', 'prayer-global-porch' ) ?></p>
                 <p><?php echo esc_html__( 'May the Lord bless your efforts, multiplying the impact of every prayer offered. May your gathering inspire a movement of extraordinary prayer warriors who will stand in the gap for the lost and see God’s kingdom expand to all nations and people groups.', 'prayer-global-porch' ) ?></p>
                 <p><?php echo esc_html__( 'We are grateful for your partnership and excited to see how God moves through your leadership.', 'prayer-global-porch' ) ?></p>
                 <p class="font-weight-bold"><?php echo esc_html__( 'Let’s mobilize prayer and watch God move!', 'prayer-global-porch' ) ?></p>
             </section>
-            <section class="container">
+            <section class="container flow-small center | mt-5">
                 <p class="font-weight-bold"><?php echo esc_html__( 'Ready...Set...', 'prayer-global-porch' ) ?></p>
-                <a class="btn btn-primary-light" href="https://prayer.global/dashboard"><?php echo esc_html__( 'Create Group Relay', 'prayer-global-porch' ) ?></a>
+                <a class="btn btn-primary-light uppercase" href="https://prayer.global/dashboard"><?php echo esc_html__( 'Create Group Relay', 'prayer-global-porch' ) ?></a>
                 <svg class="icon-xxlg"><use href="<?php echo esc_html( $svgs_url ); ?>#pg-relay"></use></svg>
             </section>
         </div>
