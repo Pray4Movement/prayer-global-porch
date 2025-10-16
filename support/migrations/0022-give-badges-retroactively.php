676d0675e72072861603bf24dcc54ac06f744754
Nath <nathinabob@gmail.com>
2025-10-16 11:18:59 +0100

    feat: add migration to give badges retroactively

---
+++ support/migrations/0022-give-badges-retroactively.php
@@ -0,0 +1,29 @@
+<?php
+if ( !defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly
+
+require_once( 'abstract.php' );
+
+class Prayer_Global_Migration_0022 extends Prayer_Global_Migration {
+    public function up() {
+        $users = get_users();
+        foreach ( $users as $user ) {
+            $has_given_badges = get_user_meta( $user->ID, 'give-badges-progress', true );
+            if ( $has_given_badges ) {
+                continue;
+            }
+
+            $badge_manager = new PG_Badge_Manager( $user->ID );
+            $newly_earned_badges = $badge_manager->get_newly_earned_badges();
+            foreach ( $newly_earned_badges as $badge ) {
+                $badge_manager->earn_badge( $badge->get_id(), retroactive: true );
+            }
+            add_user_meta( $user->ID, 'give-badges-progress', 1, true );
+        }
+    }
+
+    public function down() {}
+
+    public function test() {
+        $this->test_expected_tables();
+    }
+}
--- support/migrations/class-migration-engine.php
+++ support/migrations/class-migration-engine.php
@@ -12,7 +12,7 @@ if ( !defined( 'ABSPATH' ) ) {
 class Prayer_Global_Migration_Engine
 {
 
-    public static $migration_number = 21;
+    public static $migration_number = 22;
 
     protected static $migrations = null;
 
